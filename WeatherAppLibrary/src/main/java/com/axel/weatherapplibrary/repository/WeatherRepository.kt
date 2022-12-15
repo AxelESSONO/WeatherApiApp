ede929e98bd81f84b2077ffa0d8693a1d5463e1d
axel-essono <naxelcdric@gmail.com>
2022-12-15 15:57:03 +0100

    Setting default latitude and longitude

--- WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/repository/WeatherRepository.kt
+++ WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/repository/WeatherRepository.kt
@@ -1,6 +1,8 @@
 package com.axel.weatherapplibrary.repository
 
+import com.axel.weatherapplibrary.BuildConfig
 import com.axel.weatherapplibrary.api.WeatherApiService
+import com.axel.weatherapplibrary.utils.Constants
 import javax.inject.Inject
 
 class WeatherRepository
@@ -8,8 +10,8 @@ class WeatherRepository
 constructor(private val weatherApiService: WeatherApiService)
 {
     suspend fun getWeather(
-        latitude : Double,
-        longitude : Double,
-        apiKey : String
+        latitude : Double = Constants.DEFAULT_LAT,
+        longitude : Double = Constants.DEFAULT_LON,
+        apiKey : String = BuildConfig.WEATHER_API_KEY
     ) = weatherApiService.getWeather(latitude, longitude, apiKey)
 }
--- WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/utils/Constants.kt
+++ WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/utils/Constants.kt
@@ -2,4 +2,6 @@ package com.axel.weatherapplibrary.utils
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/onecall"
+    const val DEFAULT_LAT = 33.44
+    const val DEFAULT_LON = -94.04
 }
