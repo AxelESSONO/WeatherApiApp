e0512261a59dfeb9e2eac5cd6ec219612be84641
axel-essono <naxelcdric@gmail.com>
2022-12-19 08:49:38 +0100

    Retreive city position

--- app/src/main/java/com/axel/weatherapiapp/view/fragment/AddCityDialogFragment.kt
+++ app/src/main/java/com/axel/weatherapiapp/view/fragment/AddCityDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.axel.weatherapiapp.view.fragment
 
+import android.location.Geocoder
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -11,6 +12,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import com.axel.weatherapiapp.R
 import com.axel.weatherapiapp.databinding.FragmentAddCityDialogBinding
+import java.util.*
 
 class AddCityDialogFragment : DialogFragment() {
 
@@ -37,12 +39,29 @@ class AddCityDialogFragment : DialogFragment() {
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                     if (s?.isNotEmpty() == true){
-                        // ccc
+                        addButton.isEnabled = true
                     }
                 }
 
                 override fun afterTextChanged(s: Editable?) {}
             })
+        }
+
+        binding.addButton.setOnClickListener {
+
+            val city: String = binding.cityNameInput.text.toString()
+            val geocoder: Geocoder = Geocoder(requireContext(), Locale.getDefault())
+            val addresses = geocoder.getFromLocationName(city, 2)
+
+            val address = addresses?.get(0)
+            dismiss()
+
+            Toast.makeText(
+                requireContext(),
+                "latitude : ${address?.latitude} \nlongitude : ${address?.longitude} \nCity name : ${address?.locality}",
+                Toast.LENGTH_SHORT
+            ).show()
+
         }
 
         return binding.root
