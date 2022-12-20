8350a7c932d2123d7daace1e6967b9dd5fbe7fb5
axel-essono <naxelcdric@gmail.com>
2022-12-20 06:15:13 +0100

    Added CRUD function to handle BD

--- WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/viewmodel/WeatherViewModel.kt
+++ WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/viewmodel/WeatherViewModel.kt
@@ -1,15 +1,19 @@
 package com.axel.weatherapplibrary.viewmodel
 
+import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.axel.weatherapplibrary.dao.CityWeatherDao
+import com.axel.weatherapplibrary.database.CityWeatherDatabase
+import com.axel.weatherapplibrary.model.CityWeather
 import com.axel.weatherapplibrary.model.WeatherRoot
+import com.axel.weatherapplibrary.repository.CityWeatherRepository
 import com.axel.weatherapplibrary.repository.WeatherRepository
-import com.axel.weatherapplibrary.utils.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,10 +23,6 @@ class WeatherViewModel @Inject constructor(private val weatherRepository: Weathe
     private val _response = MutableLiveData<WeatherRoot>()
     val weatherResponse : LiveData<WeatherRoot>
         get() = _response
-
-    /*init {
-        getWeather(latitude  = Constants.DEFAULT_LAT, longitude  = Constants.DEFAULT_LON)
-    }*/
 
     private fun getWeather(latitude : Double, longitude : Double) = viewModelScope.launch {
         weatherRepository.getWeather(latitude  = latitude, longitude  = longitude).let { response ->
@@ -39,4 +39,33 @@ class WeatherViewModel @Inject constructor(private val weatherRepository: Weathe
         return weatherResponse
     }
 
+     fun addCity(context: Context, weather: CityWeather){
+        val thread = Thread{
+            val dao : CityWeatherDao  = CityWeatherDatabase.getDataBase(context).cityWeatherDao()
+            val cityWeatherRepository = CityWeatherRepository(dao)
+            cityWeatherRepository.insertCity(weather)
+        }.start()
+    }
+
+     fun getAll(context: Context) : List<CityWeather>? {
+            CityWeatherDatabase.getDataBase(context).cityWeatherDao().getAll()
+            val dao : CityWeatherDao  = CityWeatherDatabase.getDataBase(context).cityWeatherDao()
+            val cityWeatherRepository = CityWeatherRepository(dao)
+
+        return cityWeatherRepository.getAllCity()
+    }
+
+     fun deleteCity(context: Context, weather: CityWeather){
+         val thread = Thread{
+             val dbInstance = CityWeatherDatabase.getDataBase(context)
+             val dao = dbInstance.cityWeatherDao()
+             val cityWeatherRepository = CityWeatherRepository(dao)
+             cityWeatherRepository.delete(weather)
+
+         }.start()
+
+        //val db = CityWeatherDatabase.getDataBase(context).cityWeatherDao().getAll()
+
+
+    }
 }
