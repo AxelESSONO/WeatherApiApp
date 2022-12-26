a6074a79207cf0dd2f466e391d29d912598bfb24
axel-essono <naxelcdric@gmail.com>
2022-12-26 21:55:43 +0100

    Create viewmodel class for data coming from local database

---
+++ WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/viewmodel/CityWeatherViewModel.kt
@@ -0,0 +1,24 @@
+package com.axel.weatherapplibrary.viewmodel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.axel.weatherapplibrary.model.CityWeather
+import com.axel.weatherapplibrary.repository.CityWeatherRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class CityWeatherViewModel @Inject constructor(private val cityWeatherRepository: CityWeatherRepository) : ViewModel() {
+
+    fun addCity(context: Context, cityWeather: CityWeather) = viewModelScope.launch {
+        cityWeatherRepository.insertCity(cityWeather)
+    }
+
+    suspend fun getAll(context: Context) : MutableList<CityWeather>? {
+        return cityWeatherRepository.getAllCity()
+    }
+
+    fun deleteCity(context: Context, cityWeather: CityWeather) = viewModelScope.launch {
+        cityWeatherRepository.delete(cityWeather)
+    }
+}
