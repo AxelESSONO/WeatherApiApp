0d87d3fbbf52f740e2321b086291e159c098d759
axel-essono <naxelcdric@gmail.com>
2022-12-26 22:02:07 +0100

    Remove context parameter

--- WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/viewmodel/CityWeatherViewModel.kt
+++ WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/viewmodel/CityWeatherViewModel.kt
@@ -1,6 +1,5 @@
 package com.axel.weatherapplibrary.viewmodel
 
-import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.axel.weatherapplibrary.model.CityWeather
@@ -12,15 +11,15 @@ import javax.inject.Inject
 @HiltViewModel
 class CityWeatherViewModel @Inject constructor(private val cityWeatherRepository: CityWeatherRepository) : ViewModel() {
 
-    fun addCity(context: Context, cityWeather: CityWeather) = viewModelScope.launch {
+    fun addCity(cityWeather: CityWeather) = viewModelScope.launch {
         cityWeatherRepository.insertCity(cityWeather)
     }
 
-    suspend fun getAll(context: Context) : MutableList<CityWeather>? {
+    suspend fun getAll() : MutableList<CityWeather>? {
         return cityWeatherRepository.getAllCity()
     }
 
-    fun deleteCity(context: Context, cityWeather: CityWeather) = viewModelScope.launch {
+    fun deleteCity(cityWeather: CityWeather) = viewModelScope.launch {
         cityWeatherRepository.delete(cityWeather)
     }
 }
