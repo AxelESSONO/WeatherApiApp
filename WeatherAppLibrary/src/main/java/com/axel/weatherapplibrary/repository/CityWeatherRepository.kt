0f33a0d1cd85819b4cbdd2d8e940dc92fdafcdd6
axel-essono <naxelcdric@gmail.com>
2022-12-26 21:59:03 +0100

    Convert to suspend function

--- WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/repository/CityWeatherRepository.kt
+++ WeatherAppLibrary/src/main/java/com/axel/weatherapplibrary/repository/CityWeatherRepository.kt
@@ -1,11 +1,13 @@
 package com.axel.weatherapplibrary.repository
 
-import androidx.lifecycle.LiveData
 import com.axel.weatherapplibrary.dao.CityWeatherDao
 import com.axel.weatherapplibrary.model.CityWeather
+import javax.inject.Inject
 
-class CityWeatherRepository(private val cityWeatherDao: CityWeatherDao) {
-     fun insertCity(cityWeather: CityWeather) = cityWeatherDao.insertCity(cityWeather)
-     fun delete(cityWeather: CityWeather) = cityWeatherDao.delete(cityWeather)
-     fun getAllCity() : MutableList<CityWeather> = cityWeatherDao.getAll()
+class CityWeatherRepository
+@Inject constructor(private val cityWeatherDao: CityWeatherDao)
+{
+     suspend fun insertCity(cityWeather: CityWeather) = cityWeatherDao.insertCity(cityWeather)
+     suspend fun delete(cityWeather: CityWeather) = cityWeatherDao.delete(cityWeather)
+     suspend fun getAllCity() : MutableList<CityWeather> = cityWeatherDao.getAll()
 }
